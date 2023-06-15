0d8784d5c8c208c8d19882ee12695fd276d89e11
sammichaud <samuel.michaud2000@gmail.com>
2023-06-15 10:53:58 +0200

    create lend form

--- app/src/main/java/ch/cpnv/Lend.kt
+++ app/src/main/java/ch/cpnv/Lend.kt
@@ -1,6 +1,8 @@
 package ch.cpnv
 
+import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
 import android.provider.ContactsContract
@@ -8,43 +10,89 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.activity.result.ActivityResult
+import android.widget.EditText
 import androidx.fragment.app.Fragment
-import kotlin.math.log
+import android.widget.DatePicker
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import java.util.Calendar
 
 
 class Lend : Fragment() {
-    private val PICK_CONTACT_REQUEST = 1
+    private lateinit var date: EditText
+    private lateinit var textContact: TextView
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_lend, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_lend, container, false)
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        var selectContactButton = view.findViewById<Button>(R.id.select_contact_button)
+        view.findViewById<Button>(R.id.select_contact_button)
+            .setOnClickListener { pickGoogleContact() }
 
-        selectContactButton.setOnClickListener {
-            pickGoogleContact()
-        }
+        view.findViewById<Button>(R.id.btn_lend).setOnClickListener { saveLend() }
+
+
+        date = view.findViewById(R.id.select_date)
+        date.setOnClickListener { showDatePickerDialog() }
+
+        textContact = view.findViewById(R.id.contact_text)
+
+        return view
     }
 
     private fun pickGoogleContact() {
         val intent = Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI)
-        startActivityForResult(intent, PICK_CONTACT_REQUEST)
+        resultLauncher.launch(intent)
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
+    private fun saveLend() {
 
-        if (requestCode == PICK_CONTACT_REQUEST && resultCode == Activity.RESULT_OK) {
-            val contactUri = data?.data
-            val contactId = contactUri?.lastPathSegment
+    }
+
+    private fun showDatePickerDialog() {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(
+            requireContext(),
+            { _: DatePicker, selectedYear: Int, selectedMonth: Int, selectedDay: Int ->
+                // Handle the selected date
+                val selectedDate = "$selectedDay/${selectedMonth + 1}/$selectedYear"
+                date.setText(selectedDate)
+            },
+            year,
+            month,
+            day
+        )
+
+        datePickerDialog.show()
+    }
+
+    @SuppressLint("Range")
+    private val resultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data: Intent? = result.data
+                val contactUri = data?.data
+                val cursor = contactUri?.let {
+                    requireActivity().contentResolver.query(
+                        it,
+                        null,
+                        null,
+                        null,
+                        null
+                    )
+                }
+
+                cursor?.use {
+                    if (it.moveToFirst()) {
+                        textContact.text =
+                            it.getString(it.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
+                    }
+                }
+            }
         }
-    }
 }
